b99d80921250ca20a7eb749966616a9d552815e4
jamie-wearsafe <jamie@wearsafe.com>
2022-04-25 16:30:38 -0400

    update dependencies

--- scripts/format-modified-only.sh
+++ scripts/format-modified-only.sh
@@ -2,9 +2,9 @@
 # chmod +x this and save in your PATH. Assumes `prettier` is in your `devDependencies` already
 
 BASE=$(git merge-base dev HEAD) # change master to whatever your trunk branch is
-FILES=$(git diff --name-only "${BASE}" HEAD | xargs npx prettier --list-different)
+FILES=$(git diff --name-only "${BASE}" HEAD | xargs)
 echo "$FILES"
-# npx prettier --list-different $FILES
+npx prettier --list-different $FILES
 
 # Want eslint too?
 # npx eslint --ignore-path=.prettierignore $FILES
